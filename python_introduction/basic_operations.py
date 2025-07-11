ecbf72857249d83ecfc62cee52c800f24765fe64
Mukoya Kuya <deltonmukoyakuya@gmail.com>
2025-07-11 16:55:13 +0300

    Add basic_operations.py file for arithmetic task

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+sum_result = number1 + number2 
+difference_result = number1 - number2
+product_result = number * number 2
+
+print(f"Addition of {number1} and  {number2} is {sum_result}")
+print(f"Subtraction of {number1} and  {number2} is  {sum_result}")
+print(f"Multiplication of {number} and {number2} is {sum_result}")
+
